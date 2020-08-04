71305b9f250ebd435b855a76a491d945792cd0dd
hzhu <hz@henryzhu.me>
2020-08-04 00:09:54 -0700

    Update and show complete example of bug

--- src/App.js
+++ src/App.js
@@ -32,36 +32,73 @@ const FooComponent = () => {
   };
 
   return (
-    <>
-      <button onClick={onClick}>Update Foo peview</button>
-      <em> Only Foo preview should update, but both previews update :\</em>
-    </>
+    <div>
+      <button style={{ border: "3px solid red" }} onClick={onClick}>
+        Update Foo peview
+      </button>
+      <em>
+        {" "}
+        Clicking should only update Foo preview, but both previews update :\
+      </em>
+    </div>
   );
 };
 
-function App() {
+const BarComponent = () => {
   const barManagerRef = React.useRef();
+
   React.useEffect(() => {
     barManagerRef.current = new Manager(
       document.getElementById("preview-bar"),
       {
         files: {
-          "/index.js": {
+          "/main.js": {
             code: "document.body.innerHTML = '<h2>Bar preview</h2>'",
           },
         },
-        entry: "/index.js",
-        dependencies: {
-          uuid: "latest",
-        },
+        entry: "/main.js",
+        dependencies: { expect: "latest" },
       }
     );
   }, []);
 
+  const onClick = () => {
+    barManagerRef.current.updatePreview({
+      files: {
+        "/main.js": {
+          code: "document.body.innerHTML = '<h2>Bar preview UPDATED</h2>'",
+        },
+      },
+      entry: "/main.js",
+      dependencies: { expect: "latest" },
+    });
+  };
+
   return (
-    <div className="App">
-      <FooComponent />
+    <div>
+      <button style={{ border: "3px solid green" }} onClick={onClick}>
+        Update Bar peview
+      </button>
+      <em>
+        {" "}
+        Clicking should only update Bar preview, but both previews update :\
+      </em>
     </div>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <h3>
+        Foo preview should be rendered in the red iframe and Bar preview should
+        be rendered in the green iframe.
+      </h3>
+      <div className="App">
+        <FooComponent />
+        <BarComponent />
+      </div>
+    </>
   );
 }
 
